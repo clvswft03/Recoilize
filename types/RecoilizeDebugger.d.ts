4f1c18b5bbf16b3c51e5d85806d912487bbc08cc
Sumin Son <clvswft03@gmail.com>
2022-01-29 03:28:05 +0900

    feat(type): add type declarations

---
+++ types/RecoilizeDebugger.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for recoilize 0.8
+// Project: https://github.com/open-source-labs/Recoilize
+// Definitions by: Nick Bell <https://github.com/ni3t>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export type RecoilizeDebugger = (nodes: any, root: HTMLElement | null) => null;
